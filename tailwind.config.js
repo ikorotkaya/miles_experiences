451609f8287ce1bd1282d1d09ecf4123110adc32
ikorotkaya <irinkakorotkaya@gmail.com>
2023-09-06 16:03:45 +0200

    Install tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js,tsx}',
-    './components/**/*.{html,js,tsx}',
-    './index.html'
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
