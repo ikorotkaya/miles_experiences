1abfc34461ef48cf6cdec102e4241f9179c07f19
ikorotkaya <irinkakorotkaya@gmail.com>
2023-09-11 22:33:20 +0200

    Added custom Miles car icon

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { GoogleMap, LoadScript, MarkerF, InfoWindowF } from '@react-google-maps/api';
+import carMarker from "../images/car-marker.png"
 
 interface GoogleMapsComponentProps {
   userLocation: {
@@ -14,10 +15,15 @@ const GoogleMapsComponent: React.FC<GoogleMapsComponentProps> = ({ userLocation,
 
   const [selectedVenueId, setSelectedVenueId] = useState<any>(null);
   const [center, setCenter] = useState<any>(userLocation);
+  const [isGoogleMapsLoaded, setIsGoogleMapsLoaded] = useState(false);
 
   const containerStyle = {
     width: '100%',
     height: '800px',
+  };
+
+  const handleGoogleMapsLoad = () => {
+    setIsGoogleMapsLoaded(true);
   };
 
   const handleMarkerClick = (venueId: any) => {
@@ -30,10 +36,12 @@ const GoogleMapsComponent: React.FC<GoogleMapsComponentProps> = ({ userLocation,
 
   return (
     <div>
-      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''}>
+      <LoadScript 
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''}
+        onLoad={handleGoogleMapsLoad} >
         <GoogleMap mapContainerStyle={containerStyle} center={{lat: center.lat, lng: center.lng}} zoom={13} >
           {/* Add markers, polygons, or other map elements here */}
-          <MarkerF
+          {isGoogleMapsLoaded && <MarkerF
             position={userLocation}
             draggable={true}
             onDragEnd={(e) => {
@@ -41,13 +49,19 @@ const GoogleMapsComponent: React.FC<GoogleMapsComponentProps> = ({ userLocation,
                 onMarkerDragEnd({ lat: e.latLng.lat(), lng: e.latLng.lng() });
               }
             }}
-          />
+            options={{
+              icon: {
+                url: carMarker,
+                scaledSize: new window.google.maps.Size(64, 64)
+              }
+            }}
+          />}
           {venues.map((venue, index) => (
             <MarkerF 
               key={index} 
               position={venue.coordinates} 
               title={venue.name}               
-              onClick={() => handleMarkerClick(venue.id)} // Call handleMarkerClick
+              onClick={() => handleMarkerClick(venue.id)}
             >
               {selectedVenueId === venue.id && <InfoWindowF position={venue.coordinates} onCloseClick={() => handleMarkerClick(null)} options={{disableAutoPan: true}}>
                 <div>
