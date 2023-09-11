7eeaaaace62110c1082f0ab5abe2fe471a24aab8
ikorotkaya <irinkakorotkaya@gmail.com>
2023-09-11 15:21:20 +0200

    Change component name to MainContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Main from "./components/Main";
+import Main from "./components/MainContainer";
 
 
 import "./App.css";
