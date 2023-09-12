80a0de5852fac461c9642f771e3f36577614428a
ikorotkaya <irinkakorotkaya@gmail.com>
2023-09-12 09:20:20 +0200

    Change logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="bg-black w-screen">
       <div className="flex justify-between text-white h-24 items-center	my-0 mx-2.5 ">
-        <img src="images/miles-logo.jpeg" alt="miles-logo" className="h-12" />
+        <img src="images/miles-logo.svg" alt="miles-logo" className="h-20" />
         <div className="flex justify-between ">
           <Menu />
         </div>
