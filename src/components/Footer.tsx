70251e9b8396160ecc9c9e8784938ee98f3df7bf
ikorotkaya <irinkakorotkaya@gmail.com>
2023-09-16 22:47:03 +0200

    Change footer text size for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,8 @@ import React from "react";
 export default function Footer() {
   return (
     <footer id="footer" className="bg-black w-full row-start-3 col-start-1 col-span-2">
-      <div className="h-full flex text-zinc-500 text-xl items-center mx-2.5 pl-px ">
-        Made with {" "}
-        <img
-          draggable="false"
-          className="w-6 h-6 px-1 font-ibmplexmono-regular  "
-          alt="❤️"
-          src="https://twemoji.maxcdn.com/v/13.0.2/svg/2764.svg"
-        /> {" "}
-        by Irina Korotkaya for MILES Mobility GmbH in 2023
+      <div className="h-full flex text-zinc-500 md:text-xl text-s items-center mx-2.5 pl-px">
+        Made with 💚 by Irina Korotkaya for MILES Mobility GmbH in 2023
       </div>
     </footer>
   );
