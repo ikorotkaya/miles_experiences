25bfda5251f6da1a0fb3e201334f92e84d56f9e9
ikorotkaya <irinkakorotkaya@gmail.com>
2023-09-18 18:45:53 +0200

    Style dropdown menu button

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -17,12 +17,12 @@ export function Menu() {
         <img src="/images/menu.png" alt="menu-icon" />
       </div>
       {menuOpen && (
-        <div className="menu-dropdown top-full flex items-center absolute right-0 z-10 md:w-96 origin-top-right border-4 text-black md:border-black border-white md:bg-white bg-[#80DCAC] hover:border-white hover:bg-[#80DCAC] py-4 px-4 flex-col">
+        <div className="menu-dropdown top-full flex items-center absolute right-0 z-10 md:w-max w-max origin-top-right border-4 text-black md:border-black border-white md:bg-white bg-[#80DCAC] hover:border-white hover:bg-[#80DCAC] p-7">
           <a
             href="mailto:irina@korotkaya.com
             ?subject=Interview request from your portfolio website
             &body=Hi Irina, I would like to invite you for an interview."
-            className="menu-dropdown__item block text-3xl"
+            className="menu-dropdown__item block md:text-3xl text-2xl"
             onClick={closeMenu}
           >
             Invite Irina for an interview!
