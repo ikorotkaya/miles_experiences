0d724bd4b5c805c425024fa2415cd999ccb2a97d
ikorotkaya <irinkakorotkaya@gmail.com>
2023-10-06 12:02:22 +0200

    Show available languages from i18n.options.resources in LanguageDropDownMenu

--- src/components/LanguageDropDownMenu.tsx
+++ src/components/LanguageDropDownMenu.tsx
@@ -1,39 +1,31 @@
 import { useTranslation } from "react-i18next";
-import {LanguageDropDownMenuProps} from "types";
+import { LanguageDropDownMenuProps } from "types";
 
 export default function LanguageDropDownMenu({
   setMenuOpen,
 }: LanguageDropDownMenuProps){
   
   const { t, i18n } = useTranslation();
-  console.log(i18n)
-  
+  console.log(i18n.languages)
   const changeLanguage = (lng: string) => i18n.changeLanguage(lng);
 
-  const handleChange = (event: any) => {
-    const language = event.target.value;
+  const handleChange = (language: string ) => {
     changeLanguage(language);
     setMenuOpen(false);
   };
 
-  const options = [
-    { value: "en", label: "English" },
-    { value: "de", label: "Deutsch" },
-    { value: "es", label: "Español" },
-  ];
+  const availableLanguages = typeof i18n.options.resources === "object" ? Object.keys(i18n.options.resources) : [];
 
   return (
-    <div className="menu-dropdown top-full flex items-center absolute right-0 z-10 md:w-max w-max origin-top-right border-4 text-black md:border-black border-white md:bg-white p-7">
-      <select
-        onChange={handleChange}
-        className="w-32 bg-transparent border-none outline-none"
-      >
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {t(option.label)}
-          </option>
-        ))}
-      </select>
+    <div className="menu-dropdown top-full flex flex-col items-center absolute right-0 z-10 md:w-max w-max origin-top-right border-4 text-black md:border-black border-white md:bg-white p-7">
+      {availableLanguages.map((language) => (
+        <div className="flex flex-col items-center justify-center cursor-pointer"
+          key={language}
+          onClick={() => handleChange(language)}
+          >
+          {t(language)}
+        </div>
+      ))}
     </div>
   )
 }
